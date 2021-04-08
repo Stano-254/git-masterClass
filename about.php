095c36d1d0817b8aa1c08ce4f74df2e3902f9def
Stano-254 <stanoemali87@gmail.com>
2021-04-08 16:52:27 +0300

    new file after rebase

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="this is apersonal safe space to use to face my future, join me, you never know it might help you, mold yours ">
+    <meta name="keywords" content="personal space, stano, darker,stano henry, stano blog">
+    <title>Stano the Developer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <?php include_once('nav.html')?>
+    <h1 id="herotext">lets talk about the future</h1>
+
+
+
+    <footer>@2021</footer>
+</body>
+</html>
