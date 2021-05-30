6b935c5490adece2db42068b3777aad17c6dbd0e
Stano-254 <stanoemali87@gmail.com>
2021-05-30 11:37:33 +0300

    commit

--- contact.php
+++ contact.php
@@ -11,10 +11,33 @@
 </head>
 <body>
    <?php include_once('nav.html')?>
-    <h1 id="herotext">Stano Darker</h1>
-    <h2 id="herocategory">Stano Darker</h1>
+<div class="container">
+    <div class="contactinfo">
+    <h1 >get in touch with us</h1>
+    <h2>address</h2>
+    <p id="herocategory">P.O. BOX 15, 50135 <br>
+     KHWISERO <br></p>  
+    <h3>Tel: 0702303831</h3>
+    </div>
+    <div class="contactform">
+        <form action="" method="post">
+            <legend>contact us</legend>
+            <label for="email">Email</label><br>
+            <input type="email" class="formgroup" name="email" id="email"><br>
+            <label for="name">Name</label><br>
+            <input type="text"class="formgroup" name="name" id="name"><br>
+            <label for="text">Message</label><br>
+            <textarea name="text" class="formgroup" id="text" cols="30" rows="10"></textarea>
+        </form>
+    </div>
 
-
+</div>
     <footer>@2021</footer>
+<script>
+
+
+
+
+</script>
 </body>
 </html>
